411126a310ab656b1e91f7ced4ce57431e3d01a3
Christopher Scott <cscott319@gatech.edu>
2024-08-22 18:10:14 -0400

    added defaults for demo files specified for windows os

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,20 @@
-
 #define SDL_MAIN_HANDLED
 
 #include "Chip8.h"
 #include "SDL.h"
 #include <chrono>
 #include <thread>
+#include <iostream>
+#include <windows.h>
+#include <filesystem>
+#include <sstream>
+#include <codecvt>
 
-int main(int argc, char* argv[]) 
+void shutdownSDL(SDL_Window* window,SDL_Renderer* renderer) noexcept;
+std::string getDemoRom(std::wstring demo = L"IBM.ch8");
+
+int main(int argc, char* argv[])
 {
-	
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 
@@ -19,32 +25,19 @@ int main(int argc, char* argv[])
 	SDL_RenderClear(renderer);
 
 	Chip8 myChip(renderer);
-	std::string romFile{ "4-flags.ch8" };
 
-	if (argc > 1) 
-	{
-		romFile = argv[1];
-	}
-
-
-	auto shutDown = [&](int exitCode) 
-	{
-		SDL_Delay(1000);
-		SDL_DestroyWindow(window);
-		SDL_DestroyRenderer(renderer);
-		SDL_Quit();
-		return exitCode;
-	};
+	//Check if a romfile is specified, if not attempt to get a demo file
+	std::string romFile{ argc > 1 ? argv[1] : getDemoRom()};
 
 	if (!myChip.loadRom(romFile))
 	{
-		return shutDown(1);
+		shutdownSDL(window, renderer);
+		return 1;
 	}
 
 	auto now = std::chrono::steady_clock::now();
 	auto prevFrame = std::chrono::steady_clock::now();
 
-	
 	SDL_Event evt;
 	bool programRunning = true;
 	//MAIN LOOP
@@ -54,26 +47,62 @@ int main(int argc, char* argv[])
 		{
 			switch (evt.type)
 			{
-				case SDL_QUIT:  
-					programRunning = false; 
-					break;
+			case SDL_QUIT:
+				programRunning = false;
+				break;
 			}
 		}
-	
+
 		now = std::chrono::steady_clock::now();
-	
+
 		if (!myChip.run())
 		{
 			programRunning = false;
 		}
-	
+
 		std::chrono::duration<double, std::milli> delta = now - prevFrame;
-	
+
 		prevFrame = now;
 		std::chrono::duration<double, std::milli> delta_ms(700.0 - delta.count());
 		auto delta_ms_duration = std::chrono::duration_cast<std::chrono::milliseconds>(delta_ms);
 		std::this_thread::sleep_for(std::chrono::milliseconds(delta_ms_duration.count()));
-
 	}
-	return shutDown(0);
+	shutdownSDL(window, renderer);
+	return 0;
+}
+
+
+static void shutdownSDL(SDL_Window* window, SDL_Renderer* renderer) noexcept
+{
+	if (renderer)
+	{
+		SDL_DestroyRenderer(renderer);
+	}
+	if (window)
+	{
+		SDL_DestroyWindow(window);
+	}
+	SDL_Quit();
+}
+
+static std::string getDemoRom(std::wstring demo)
+{
+	wchar_t buffer[MAX_PATH];
+	GetModuleFileNameW(NULL, buffer, MAX_PATH);
+	std::wcout << buffer << '\n';
+	std::filesystem::path exePath(buffer);
+	std::wostringstream wos;
+	auto pathToDemo = exePath.parent_path().parent_path().parent_path().parent_path();
+	pathToDemo /= std::filesystem::path(L"Demo\\" + demo);
+	std::wstring demoPathString = pathToDemo.wstring();
+	if (!std::filesystem::exists(pathToDemo))
+	{
+		std::wcerr << "The specified demo: " << demoPathString << " was not found!" << '\n';
+		return "N/A";
+	}
+
+	using convert_type = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_type, wchar_t> converter;
+	std::string stdDemoPathString = converter.to_bytes(demoPathString);
+	return stdDemoPathString;
 }
